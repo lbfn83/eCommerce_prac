c200ce58391765f75b95e8718d4bdc08deb69b0e
lbfn83 <lbfn83@gmail.com>
2022-11-12 21:34:23 -0500

    my first play ground for session

---
+++ controllers/auth.js
@@ -0,0 +1,20 @@
+exports.getLogin = (req, res, next) => {
+  //   const isLoggedIn = req
+  //     .get('Cookie')
+  //     .split(';')[1]
+  //     .trim()
+  //     .split('=')[1] === 'true';
+  console.log(req.session);
+  console.log(req.session.isLoggedIn);
+  res.render('auth/login', {
+    path: '/login',
+    pageTitle: 'Login',
+    isAuthenticated: req.session.isLoggedIn
+  });
+};
+
+exports.postLogin = (req, res, next) => {
+  // you can add any key here
+  req.session.isLoggedIn = true;
+  res.redirect('/');
+};
---
+++ models/session.js
@@ -0,0 +1,23 @@
+
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const sessionSchema = new Schema({
+  _id: {
+    type: String,
+    required: true
+  },
+  expires: {
+    type: Date,
+    required: true
+  },
+  // https://stackoverflow.com/questions/42019679/object-type-in-mongoose
+  session: {
+    type : Schema.Types.Mixed,
+    required : true
+
+  }
+});
+
+module.exports = mongoose.model('Session', sessionSchema);
